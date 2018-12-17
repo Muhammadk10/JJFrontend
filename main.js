059b5dc546fd40ee17f88132b53b5c4ffe1169ac
Muhammad Khan <uzikhan10@gmail.com>
2018-12-17 17:14:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+var temp = 2;
+
+var tempstring = "2"
+
+
+//hello
+
+if (temp === tempstring)
+
+
+{
+tempstring()
+}
+
+
+var _count = 0;
+function increment()
+{
+    console.log(_count++);
+}
+
+
+function decrement()
+{
+    console.log(_count--)
+}
+
+
+console.log(temp == tempstring)
